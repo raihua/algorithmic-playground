3bea70dcb480a2f119c9683061933c17f526601f
raihua <raihua.dev@gmail.com>
2023-10-25 01:56:05 +1030

    Add: comments for insertion sort

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -1,16 +1,15 @@
-def insertionSort(arr):
+def insertion_sort(arr):
+    # Start from the second element because the first element is considered sorted.
     for i in range(1, len(arr)):
         key = arr[i]
         j = i - 1
-        
 
+        # Move elements that are greater than the key to one position ahead of their current position.
         while j >= 0 and arr[j] > key:
             arr[j + 1] = arr[j]
             j -= 1
-        arr[j + 1] = key
-    
-    return arr
 
-data = [5, 2, 4, 6, 1, 3]
-sortedData = insertionSort(data)
-print(sortedData)
+        # Place the key in its correct position within the sorted part of the array.
+        arr[j + 1] = key
+
+    return arr
